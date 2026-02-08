f7eb43a4613d0ad13fa76bda4777bf8e779d4c48
leeho7029 <80402477+leeho7029@users.noreply.github.com>
2026-02-08 14:45:22 +0900

    [Bronze V] Title: 새싹, Time: 84 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r\'\"7"+"\n");
+        sb.append("r`-_   ,'  ,/"+"\n");
+        sb.append(" \\").append(". \". L_r'"+"\n");
+        sb.append("   `~").append("\\").append("/"+"\n");
+        sb.append("      |"+"\n");
+        sb.append("      |");
+        System.out.print(sb);
+         
+    }
+}
