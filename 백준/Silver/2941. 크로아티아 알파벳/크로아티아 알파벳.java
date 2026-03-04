33a6591fc13e28ec90f220419275934c8d755264
leeho7029 <80402477+leeho7029@users.noreply.github.com>
2026-03-04 11:19:57 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -2,20 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String a = br.readLine();
-        a= a.replace("dz=","*");
-        a= a.replace("c=","*");
-        a= a.replace("c-","*");
-        a= a.replace("d-","*");
-        a= a.replace("lj","*");
-        a= a.replace("nj","*");
-        a= a.replace("s=","*");
-        a= a.replace("z=","*");
-        
-        System.out.println(a.length());
-        
-    
+        String a = br.readLine(); 
+        int cnt = 0;
+        for(int i  = 0; i < a.length(); i++){
+            if(i + 2 < a.length() && a.substring(i,i+3).equals("dz=")){
+                cnt++;
+                i += 2;
+            }else if (i + 1 < a.length()) {
+                String two = a.substring(i, i + 2);
+
+                if (two.equals("c=") || two.equals("c-") ||
+                    two.equals("d-") || two.equals("lj") ||
+                    two.equals("nj") || two.equals("s=") ||
+                    two.equals("z=")) {
+
+                    cnt++;
+                    i += 1;
+                } else {
+                    cnt++;
+                }
+            }else{
+                cnt++;
+            };
+        }
+        System.out.println(cnt);
+       
     }
 }
