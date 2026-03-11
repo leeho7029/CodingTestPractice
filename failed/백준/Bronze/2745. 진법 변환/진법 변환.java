45b2f88316e599ad283470e2dc1e10186dcdd5be
leeho7029 <80402477+leeho7029@users.noreply.github.com>
2026-03-11 22:37:33 +0900

    [Bronze II] Title: 진법 변환, Time:  ms, Memory:  KB -BaekjoonHub

--- failed/백준/Bronze/2745. 진법 변환/진법 변환.java
+++ failed/백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -7,9 +7,9 @@ public class Main{
         StringTokenizer st = new StringTokenizer(br.readLine());
         String a = st.nextToken();
         int b = Integer.parseInt(st.nextToken());
-        long result = 0;
+        
         for(int i = 0; i < a.length; i++){
-            char ch = a.charAt(i);
+            char ch = n.charAt(i);
             int value;
             if (ch >= '0' && ch <= '9') {
                 value = ch - '0';
