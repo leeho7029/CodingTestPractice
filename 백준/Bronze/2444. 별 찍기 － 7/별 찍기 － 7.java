0b64f2a0a28a89d0fa5190814b5d7f81334ea82d
leeho7029 <80402477+leeho7029@users.noreply.github.com>
2026-03-11 22:43:50 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 112 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i = 1; i < n+1  ; i++){
+            for(int j = 0; j < n -i ; j++){
+                bw.write(" ");
+            }
+            for(int l = 0; l < 2*i -1 ; l++){
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        for(int i = n-1; i > 0 ; i--){
+            for(int j = 0; j < n -i ; j++){
+                bw.write(" ");
+            }
+            for(int l = 0; l < 2*i -1 ; l++){
+                bw.write("*");
+            }
+                bw.write("\n");
+        }
+        bw.flush();
+    }
+}
