2bab6b7fbe8a6d0717e73e65cd568ada4fcda6df
leeho7029 <80402477+leeho7029@users.noreply.github.com>
2026-03-17 14:07:04 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int end = 1;
+        int count = 1;
+        while(a>end){
+            end += 6*count;
+            count++;
+        }
+        System.out.println(count);
+        
+    }
+}
